2261d6c5ed9d6b52e160f85a2db9af3d695de393
jamesjmeyer210 <jamesjmeyer210@gmail.com>
2019-02-14 14:24:44 -0600

    Added search servlet

---
+++ src/main/java/com/codeup/adlister/controllers/SearchServlet.java
@@ -0,0 +1,24 @@
+package com.codeup.adlister.controllers;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "controllers.SearchServlet", urlPatterns = "/search")
+public class SearchServlet extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+        throws ServletException, IOException
+    {
+        System.out.println("DEBUG: /search : doGet(...)");
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+        throws IOException
+    {
+        System.out.println("DEBUG: /search : doPost(...)");
+    }
+}
