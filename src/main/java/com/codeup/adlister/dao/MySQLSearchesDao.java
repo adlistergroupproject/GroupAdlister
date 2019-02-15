3a62e5e37ec2e08a0a579a197c03bff7a091cebc
jamesjmeyer210 <jamesjmeyer210@gmail.com>
2019-02-15 10:02:14 -0600

    Added methods to inteface

--- src/main/java/com/codeup/adlister/dao/MySQLSearchesDao.java
+++ src/main/java/com/codeup/adlister/dao/MySQLSearchesDao.java
@@ -1,9 +1,26 @@
 package com.codeup.adlister.dao;
 
+import java.util.List;
+
 public class MySQLSearchesDao extends MySQLAdsDao implements Searches {
 
     public MySQLSearchesDao(Config config){
         super(config);
     }
 
+    public List<Ads> search(String titles, String description){
+        return null;
+    }
+
+    public List<Ads> searchTitles(String titles){
+        return null;
+    }
+
+    public List<Ads> searchDescriptions(String descriptions){
+        return null;
+    }
+
+    public List<Ads> searchCategories(String[] categories){
+        return null;
+    }
 }
--- src/main/java/com/codeup/adlister/dao/Searches.java
+++ src/main/java/com/codeup/adlister/dao/Searches.java
@@ -1,7 +1,15 @@
 package com.codeup.adlister.dao;
 
+import java.util.List;
+
 public interface Searches {
 
+    public List<Ads> search(String titles, String description);
 
+    public List<Ads> searchTitles(String titles);
+
+    public List<Ads> searchDescriptions(String descriptions);
+
+    public List<Ads> searchCategories(String[] categories);
 
 }
