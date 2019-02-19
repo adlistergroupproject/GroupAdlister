54ffd3ef496b6150d334f13762099fd562bb2d42
jamesjmeyer210 <jamesjmeyer210@gmail.com>
2019-02-19 12:09:57 -0600

    Added methods to category class

--- src/main/java/com/codeup/adlister/models/Category.java
+++ src/main/java/com/codeup/adlister/models/Category.java
@@ -7,5 +7,42 @@ public class Category {
 
     private static final Categories categories = DaoFactory.getCategoriesDao();
 
+    private String key;
+    private String value;
+    private int index;
+
+    public Category(String key, String value, int index){
+        this.key = key;
+        this.value = value;
+        this.index = index;
+    }
+
+    public Category(String key){
+        this.key = key;
+        this.value = this.categories.getCategory(key);
+        this.index = -1; // TODO: change this... not that important
+    }
+
+    public Category(){
+        this.index = (int)(Math.floor(Math.random() * 1000)) % categories.getKeys().size();
+        this.value = categories.getCategory(index);
+        this.key = null; // TODO: change this... not that important
+    }
+
+    public String getValue(){
+        return this.value;
+    }
+
+    public int getId(){
+        return this.index;
+    }
+
+    public String getKey(){
+        return this.key;
+    }
+    
+    public String toString(){
+        return "id: " + this.index + ", key: " + this.key + ", value:" + this.value;
+    }
 
 }
