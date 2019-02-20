c46b5fc0ebf629e46c3538539eea345f9a64d94f
jamesjmeyer210 <jamesjmeyer210@gmail.com>
2019-02-20 15:26:09 -0600

    index.js watches the registration fields

--- src/main/webapp/WEB-INF/register.jsp
+++ src/main/webapp/WEB-INF/register.jsp
@@ -29,5 +29,7 @@
             <input type="submit" class="btn btn-primary btn-block">
         </form>
     </div>
+    <script type="text/javascript" src="${pageContext.request.contextPath}/js/jquery.js"></script>
+    <script type="text/javascript" src="${pageContext.request.contextPath}/js/index.js"></script>
 </body>
 </html>
--- src/main/webapp/js/index.js
+++ src/main/webapp/js/index.js
@@ -4,15 +4,42 @@
 const lettersAndSpaces = (str) => {
     return /^[a-zA-Z ]+$/.test(str);
 };
-
+// returns true if a string contains only lower case and upper case letters
 const lettersOnly = (str) => {
     return /^[a-zA-Z]+$/.test(str);
 };
+
+const validEmailAddr = (str) => {
+    return (str.contains("@") && str.contains(".") && !str.contains(' '));
+};
+
+const caseWarning = "Please only enter lowercase or uppercase letters";
 
 // watch the search bar for keyboard events
 $('#search-bar').keyup((event) => {
     let search = $('#search-bar').val();
     if(!lettersAndSpaces(search) && search !== ""){
-        alert("Please enter only lower case or upper case letters.");
+        alert(caseWarning);
+    }
+});
+
+// watch the froms on the register page
+$('#register-username').keyup(() => {
+   let username = $('#register-username').val();
+   if(!lettersOnly(username) && username !== ""){
+       alert(caseWarning);
+   }
+});
+
+// watch the password confirmation text field
+$('#register-confirm-password').keyup(() => {
+    let originalPass = $('#register-password').val();
+    let confirmPass = $('#register-confirm-password').val();
+    console.log(`${originalPass} ${confirmPass}`);
+
+    if(confirmPass.length >= originalPass.length){
+        if(confirmPass !== originalPass){
+            alert("Passwords do not match.");
+        }
     }
 });
