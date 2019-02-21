a388716ec136162162d4c41ea67add9ad066921c
Sam Rodriguez <sam27rodriguez@gmail.com>
2019-02-21 09:41:32 -0600

    added the navbarservlet

---
+++ src/main/java/com/codeup/adlister/controllers/NavBarServlet.java
@@ -0,0 +1,16 @@
+package com.codeup.adlister.controllers;
+import com.codeup.adlister.dao.DaoFactory;
+import com.codeup.adlister.models.User;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+@WebServlet(name = "controllers.NavBarServlet")
+public class NavBarServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("/partials/navbar.jsp").forward(request, response);
+    }
+}
